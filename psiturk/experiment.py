df5f2d741f8414f3392ad8c8bb4ead2919f65781
JSlote <joseph.slote@gmail.com>
2014-08-05 14:26:33 -0500

    change order

--- psiturk/experiment.py
+++ psiturk/experiment.py
@@ -636,7 +636,6 @@ def ppid():
 # Insert "mode" into pages so it's carried from page to page done server-side
 # to avoid breaking backwards compatibility with old templates.
 def insert_mode(page_html, mode):
-    page_html = page_html.decode("utf-8")
     ''' Insert mode '''
     page_html = page_html.decode("utf-8")
     match_found = False
